f53d279f21249c5b81bf540ccca89c300107b633
Deborah Castiglioni <debjgorton@gmail.com>
2019-02-03 16:41:15 -0600

    Add place holder layout for main content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Header from "./Components/Header";
+import OpenBook from "./Components/OpenBook";
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <OpenBook />
       </div>
     );
   }
---
+++ src/Components/OpenBook.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default class OpenBook extends Component {
+  render() {
+    return (
+        <div className="OpenBook">
+        <h2>Video goes here</h2>
+        <p className="about-vid">About video here</p>
+
+        <h2>Video goes here</h2>
+        <p className="about-vid">About video here</p>
+
+        <h2>Video goes here</h2>
+        <p className="about-vid">About video here</p>
+    </div>
+    );
+  }
+}
