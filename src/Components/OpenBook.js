b2c66616c2e47d309938235a461026b7a6b9f275
Deborah Castiglioni <debjgorton@gmail.com>
2019-02-06 15:33:07 -0600

    add scss

--- src/Components/OpenBook.js
+++ src/Components/OpenBook.js
@@ -3,15 +3,22 @@ import React, { Component } from "react";
 export default class OpenBook extends Component {
   render() {
     return (
-        <div className="OpenBook">
-        <h2>Video goes here</h2>
-        <p className="about-vid">About video here</p>
+      <div className="OpenBook">
 
+        <div className="OpenBook__about"> 
         <h2>Video goes here</h2>
-        <p className="about-vid">About video here</p>
+        <p>About video here</p>
+        </div>
 
+        <div className="OpenBook__about"> 
         <h2>Video goes here</h2>
-        <p className="about-vid">About video here</p>
+        <p>About video here</p>
+        </div>
+
+        <div className="OpenBook__about"> 
+        <h2>Video goes here</h2>
+        <p>About video here</p>
+        </div>
     </div>
     );
   }
